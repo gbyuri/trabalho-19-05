1c61845c23c0fe39f4cbc2f9f851ffbd9f6051bf
Gabriel Oliveira <117647436+gbyuri@users.noreply.github.com>
2023-05-19 11:49:28 -0300

    Add files via upload

---
+++ Qualidade de Software/Projeto 19-05-2023/js/script.js
@@ -0,0 +1,12 @@
+addEventListener('load', function banner() {
+    console.log("teste")
+    document.querySelector('#img-1').checked = true;
+    console.log(document.querySelector('#img-1').checked)
+})
+
+
+function checkbanner() {
+    addEventListener('click', function check(){
+       
+    })
+}
