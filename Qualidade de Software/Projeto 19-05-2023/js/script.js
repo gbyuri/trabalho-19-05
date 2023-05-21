6dbe737d9e20f0c19d546ecece1bfb98ebac7e12
Gabriel Oliveira <117647436+gbyuri@users.noreply.github.com>
2023-05-21 16:42:46 -0300

    Add files via upload

--- Qualidade de Software/Projeto 19-05-2023/js/script.js
+++ Qualidade de Software/Projeto 19-05-2023/js/script.js
@@ -1,12 +1,78 @@
-addEventListener('load', function banner() {
-    console.log("teste")
+
+let i = 0;
+
+window.addEventListener('load', function banner() {
     document.querySelector('#img-1').checked = true;
-    console.log(document.querySelector('#img-1').checked)
+    document.querySelector('.banner-img1').style.display = "block";
 })
 
-
-function checkbanner() {
+function checkbanner1() {
     addEventListener('click', function check(){
-       
+        document.querySelector('#img-2').checked = false;
+        document.querySelector('#img-3').checked = false;
+        document.querySelector('#img-4').checked = false;
+        document.querySelector('#img-5').checked = false;
+        document.querySelector('#img-1').checked = true;
+        document.querySelector('.banner-img1').style.display = "block";
+        document.querySelector('.banner-img2').style.display = "none";
+        document.querySelector('.banner-img3').style.display = "none";
+        document.querySelector('.banner-img4').style.display = "none";
+        document.querySelector('.banner-img5').style.display = "none";
+    })
+}
+function checkbanner2() {
+    addEventListener('click', function check(){
+        document.querySelector('#img-1').checked = false;
+        document.querySelector('#img-3').checked = false;
+        document.querySelector('#img-4').checked = false;
+        document.querySelector('#img-5').checked = false;
+        document.querySelector('#img-2').checked = true;
+        document.querySelector('.banner-img2').style.display = "block";
+        document.querySelector('.banner-img1').style.display = "none";
+        document.querySelector('.banner-img3').style.display = "none";
+        document.querySelector('.banner-img4').style.display = "none";
+        document.querySelector('.banner-img5').style.display = "none";
+    })
+}
+function checkbanner3() {
+    addEventListener('click', function check(){
+        document.querySelector('#img-1').checked = false;
+        document.querySelector('#img-2').checked = false;
+        document.querySelector('#img-4').checked = false;
+        document.querySelector('#img-5').checked = false;
+        document.querySelector('#img-3').checked = true;
+        document.querySelector('.banner-img3').style.display = "block";
+        document.querySelector('.banner-img1').style.display = "none";
+        document.querySelector('.banner-img2').style.display = "none";
+        document.querySelector('.banner-img4').style.display = "none";
+        document.querySelector('.banner-img5').style.display = "none";
+    })
+}
+function checkbanner4() {
+    addEventListener('click', function check(){
+        document.querySelector('#img-1').checked = false;
+        document.querySelector('#img-2').checked = false;
+        document.querySelector('#img-3').checked = false;
+        document.querySelector('#img-5').checked = false;
+        document.querySelector('#img-4').checked = true;
+        document.querySelector('.banner-img4').style.display = "block";
+        document.querySelector('.banner-img1').style.display = "none";
+        document.querySelector('.banner-img2').style.display = "none";
+        document.querySelector('.banner-img3').style.display = "none";
+        document.querySelector('.banner-img5').style.display = "none";
+    })
+}
+function checkbanner5() {
+    addEventListener('click', function check(){
+        document.querySelector('#img-1').checked = false;
+        document.querySelector('#img-2').checked = false;
+        document.querySelector('#img-3').checked = false;
+        document.querySelector('#img-4').checked = false;
+        document.querySelector('#img-5').checked = true;
+        document.querySelector('.banner-img5').style.display = "block";
+        document.querySelector('.banner-img1').style.display = "none";
+        document.querySelector('.banner-img2').style.display = "none";
+        document.querySelector('.banner-img3').style.display = "none";
+        document.querySelector('.banner-img4').style.display = "none";
     })
 }
